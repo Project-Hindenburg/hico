6566b3d88aa5cb902de0909a80ce82faec0a87b3
vale <destasiovaleria16@gmail.com>
2026-02-12 16:48:50 +0100

    wrong model

--- scripts/main.py
+++ scripts/main.py
@@ -5,13 +5,13 @@ import numpy as np
 import torch
 
 
-# Download and infer on model Llama-3.2-1B
+# Download and infer on model Llama3.1-8B
 
 # For each of the dataset files do:
 
     # Pass to LLama the dataset file as the prompt
 
-    # Download the hidden representation at various layers: FOCUS ON LAYER 26 AS THEY SAW IT GIVES THE BEST RESULTS
+    # Download the hidden representation of the last 50 tokens at various layers: FOCUS ON LAYER 26 AS THEY SAW IT GIVES THE BEST RESULTS
 
     # Visualize the hidden representation using PCA (with 2/3 principal components): tokens representing the same words should be coloured the same
 
