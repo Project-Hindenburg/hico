fa2ccf5f95c9fef4178379749399b99b0d8e8967
gluc <gio.lucarelli99@gmail.com>
2026-02-19 13:12:22 +0100

    tok test

---
+++ orfeo/tokenizer_test.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+from transformers import AutoTokenizer
+
+
+MODEL_DIR = os.environ["MODEL_DIR"]
+TEXT_PATH = Path(os.environ["TEXT_PATH"])
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR, use_fast=True)
+
+text = TEXT_PATH.read_text(encoding="utf-8")
+
+# LLaMA-style: nessun pad_token di default
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+
+token_ids = tokenizer(text, 
+                      return_tensors="pt",
+                      padding=True,
+                      truncation=False,
+                      return_attention_mask=True,
+                      add_special_tokens=False
+                      )["input_ids"][0]
+
+token_str = tokenizer.convert_ids_to_tokens(token_ids.tolist())
+
+print("Token IDs shape:\n", token_ids.shape)
+print("\nToken IDs:\n", list(token_ids))
+print("\nToken Strings shape:\n", len(token_str))
+print("\nToken Strings:\n", token_str)
